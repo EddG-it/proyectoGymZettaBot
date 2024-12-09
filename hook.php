d4b2a072b008f576b2fcf8a48444cf59d0155432
EddG-it <20201223@tecnmtlahuac.onmicrosoft.com>
2024-12-09 16:29:44 -0600

    se agregan los archivos set.php y hook.php

--- hook.php
+++ hook.php
@@ -1,28 +1,19 @@
 <?php
 
-use Longman\TelegramBot\Entities\Chat;
-
 require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key = '7663270134:AAFgp7_TrVm6qNtDcPQn9guq3xDsYWIUYTo';
 $bot_username = '@gymZettaNewBot';
-$hook_url = 'https://mintcream-rook-782507.hostingersite.com/set.php';
 
 try{
 
     $telegram = new \Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
-    $result = $telegram->setWebhook($hook_url);
-
-    if ($result->isOk()){
-        
-        echo $result->getDescription();
-
-    }
+    $telegram->handle();
 }catch(\Longman\TelegramBot\Exception\TelegramException $e){
 
     #log telegram errors
-    echo $e->getMessage();
+    #echo $e->getMessage();
 }
 
 
