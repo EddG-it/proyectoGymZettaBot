66516d8aa140dd638382644b9ef6b8c051118b89
EddG-it <20201223@tecnmtlahuac.onmicrosoft.com>
2024-12-09 21:39:40 -0600

    se agregan los comandos en /vendor/longman/telegram-bot/src/Commands para llamarlos desde hook.php

--- hook.php
+++ hook.php
@@ -9,6 +9,15 @@ try{
 
     $telegram = new \Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
 
+    $telegram->addCommandsPath(__DIR__ . '/vendor/longman/telegram-bot/src/Commands');
+
+    $commands = $telegram->getCommandsList();
+
+    foreach ($commands as $name=>$command){
+
+        echo"Comando: $name - {$command->getDescription()}" . PHP_EOL;
+    }
+
     $telegram->handle();
 }catch(\Longman\TelegramBot\Exception\TelegramException $e){
 
